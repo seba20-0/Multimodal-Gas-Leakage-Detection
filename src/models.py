8cb98319247391c54d79b8c667181276e6849d7f
Seba Al Mokdad <60008555+seba20-0@users.noreply.github.com>
2025-05-03 23:12:18 +0400

    Fix dropout

--- src/models.py
+++ src/models.py
@@ -42,6 +42,7 @@ class MCDropout(tf.keras.layers.Dropout):
 def build_sensor_model(
     input_dim=7,
     sensor_dropout_rate=0.3,
+    layer_dropout = 0.5,
     hidden_units=(64, 32),
     output_units=4,
     lr=1e-4
@@ -52,9 +53,9 @@ def build_sensor_model(
     inp = tf.keras.Input(shape=(input_dim,), name="sensor_input")
     x = RandomSensorDropout(sensor_dropout_rate, name="sensor_dropout")(inp)
     x = tf.keras.layers.Dense(hidden_units[0], activation="relu")(x)
-    x = MCDropout(0.5)(x)
+    x = MCDropout(layer_dropout)(x)
     x = tf.keras.layers.Dense(hidden_units[1], activation="relu")(x)
-    x = MCDropout(0.5)(x)
+    x = MCDropout(layer_dropout)(x)
     out = tf.keras.layers.Dense(output_units, activation="softmax", name="output")(x)
 
     model = tf.keras.Model(inp, out, name="sensor_only")
@@ -71,6 +72,7 @@ def build_sensor_model(
 def build_image_model(
     img_shape=(120,160,3),
     conv_filters=(32, 64),
+    layer_dropout = 0.5,
     dense_units=64,
     output_units=4,
     lr=1e-4
@@ -84,7 +86,7 @@ def build_image_model(
     x = tf.keras.layers.Conv2D(conv_filters[1], 3, activation="relu", padding="same")(x)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dense(dense_units, activation="relu")(x)
-    x = MCDropout(0.5)(x)
+    x = MCDropout(layer_dropout)(x)
     out = tf.keras.layers.Dense(output_units, activation="softmax", name="output")(x)
 
     model = tf.keras.Model(inp, out, name="image_only")
@@ -102,6 +104,7 @@ def build_multimodal_model(
     img_shape=(120,160,3),
     input_dim=7,
     sensor_dropout_rate=0.3,
+    layer_dropout = 0.5,
     sensor_units=32,
     img_dense=64,
     fusion_dense=64,
@@ -117,7 +120,7 @@ def build_multimodal_model(
     # Sensor branch
     s = RandomSensorDropout(sensor_dropout_rate, name="sensor_dropout")(s_in)
     s = tf.keras.layers.Dense(sensor_units, activation="relu")(s)
-    s = MCDropout(0.5)(s)
+    s = MCDropout(layer_dropout)(s)
 
     # Image branch
     x = tf.keras.layers.Conv2D(32, 3, activation="relu", padding="same")(i_in)
@@ -125,12 +128,12 @@ def build_multimodal_model(
     x = tf.keras.layers.Conv2D(64, 3, activation="relu", padding="same")(x)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dense(img_dense, activation="relu")(x)
-    x = MCDropout(0.5)(x)
+    x = MCDropout(layer_dropout)(x)
 
     # Fusion
     fused = tf.keras.layers.Concatenate()([s, x])
     y = tf.keras.layers.Dense(fusion_dense, activation="relu")(fused)
-    y = MCDropout(0.5)(y)
+    y = MCDropout(layer_dropout)(y)
     out = tf.keras.layers.Dense(output_units, activation="softmax", name="output")(y)
 
     model = tf.keras.Model([s_in, i_in], out, name="multimodal")
